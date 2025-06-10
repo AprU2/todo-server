39d4e6bbe0cdd250e8c8d66d318a1e83dbddc8ab
AprU2 <>
2025-06-10 11:22:09 +0300

    improve CORS

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') || 4000;
+  const frontendUrl = configService.get<string>('FRONTEND_URL');
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: (origin, callback) => {
-      if (!origin || origin.startsWith('http://localhost')) {
+      if (!origin || origin.startsWith(frontendUrl)) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
@@ -18,6 +24,6 @@ async function bootstrap() {
     allowedHeaders:
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   });
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
